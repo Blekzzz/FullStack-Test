78f77c92926e4df6dba5460eba7f516a57f06376
NathanzaGaurangga <nathnzagaurangga@gmail.com>
2023-11-11 02:37:17 +0800

    deploy commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/router')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
